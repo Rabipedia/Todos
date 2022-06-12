0cc9ea90d0f4344151ca008060e469b363bcb569
Rabiul hossain <mohammadrabiul4@gmail.com>
2022-06-12 08:26:07 -0700

    list view added.

---
+++ src/components/todos/listview/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { ListGroup } from 'reactstrap';
+import ListItem from "./listitem";
+
+const ListView = ({ todos, toggleSelect, toggleComplete }) => {
+    return (
+        <ListGroup>
+            {
+                todos.map(todo => (
+                    <ListItem
+                        todo={todo}
+                        toggleSelect={toggleSelect}
+                        toggleComplete={toggleComplete}
+                    />
+                ))
+            }
+        </ListGroup>
+    )
+}
+
+ListGroup.propTypes = {
+    todos: PropTypes.object.isRequired,
+    toggleSelect: PropTypes.func.isRequired,
+    toggleComplete: PropTypes.func.isRequired
+}
+
+export default ListGroup;
